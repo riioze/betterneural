d6fad5e430023e5698d71539ed2555f59af9ee0d
riioze <61903425+riioze@users.noreply.github.com>
2020-11-28 16:25:42 +0100

    0.0.5
    He can forward in the ann

--- betterneural/networks.py
+++ betterneural/networks.py
@@ -1,22 +1,32 @@
 import numpy as np
-
+import math
 ACTIVATIONLIST = ['relu','sigmoid']
 
 def get_activation():
     """return the list of choosable activation fonctions"""
     return ACTIVATIONLIST
 
+def relu(x):
+    return np.maximum(0,x)
 
-class layer:
+
+
+class layerDense:
     def __init__(self,inputs,nodes,activation):
         """create a layer
         inputs : an int for the number of inputs
         nodes : an int for the number of node
         activation : a str for the activation fonction use get_activation() to have the list of activation fonctions
         """
+        self.activation = activation
         self.nodes = nodes
-        self.weights = np.random.rand(nodes,inputs)
-        self.biases = np.random.rand(nodes,1)
+        self.weights = np.random.uniform(-1,1,(inputs,nodes))
+        self.biases = np.zeros((1,nodes))
+
+    def forward(self,inputs):
+        """go forward through the layer and transform inputs to outputs with a dot pruduct between inputs and weights + biases""" 
+        self.output = self.activation(np.dot(inputs,self.weights)+self.biases)
+        return self.output
 
 class ANN:
     def __init__(self,ninputs):
@@ -27,17 +37,24 @@ class ANN:
         self.layers = []
         self.ninputs = ninputs
 
-    def add(self,nodes,activation = 'relu'):
+    def add(self,nodes,activation = relu):
         """Add a layer in the network
         nodes : an int for the number of node
-        activation : a str for the activation fonction use get_activation() to have the list of activation fonctions
+        activation : a function for the activation fonction use get_activation() to have the list of activation fonctions
         """
         if self.layers == []:
-            l = layer(self.ninputs,nodes,activation)
+            l = layerDense(self.ninputs,nodes,activation)
         else:
-            l = layer(self.layers[-1].nodes,nodes,activation)
+            l = layerDense(self.layers[-1].nodes,nodes,activation)
 
         self.layers.append(l)
+
+    def forward(self,inputs):
+        """loop through the layers to transform the input into an output"""
+        current = inputs
+        for l in self.layers:
+            current = l.forward(current)
+        return current
 
 
 
